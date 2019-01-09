90ed323684bd86b956eb04131dc5c036470934af
Ahmad Alali (Migrate) <a-ahalal@microsoft.com>
2019-01-09 17:25:51 +0200

    update database settings

--- app.js
+++ app.js
@@ -1,16 +1,53 @@
 const http = require('http');
-const port=process.env.PORT || 3000
+const mysql = require('mysql');
+const port = process.env.PORT || 8997;
+const connectionString = process.env.MYSQLCONNSTR_localdb;
+
+var queryResult;
+//Database=localdb;Data Source=127.0.0.1:49833;User Id=azure;Password=6#vWHD_$
+
+
+// var arr = connectionString.split(";");
+// var arr2 = arr[1].split(":");
+// var result = "";
+
+var con = mysql.createConnection({
+
+//Database=localdb;Data Source=127.0.0.1:49833;User Id=azure;Password=6#vWHD_$
+
+    // host: arr2[0],
+    // port: arr2[1],
+    // user: arr[2],
+    // password: arr[3],
+    // database: arr[0]
+    
+    host: "127.0.0.1",
+    port: "49833",
+    user: "azure",
+    password: "6#vWHD_$",
+    database: "localdb"
+
+  });
+
+  con.connect(function(err) {
+    if (err) throw err;
+    con.query("SELECT * FROM users", function (err, result, fields) {
+      if (err) throw err;
+       queryResult = JSON.stringify(result);
+      console.log(result);
+    });
+  });
 
 const server = http.createServer((req, res) => {
 
 res.statusCode = 200;
 res.setHeader('Content-Type', 'text/html');
-res.end('<h1>Hello World</h1>');
+res.end(queryResult);
 
 });
 
 server.listen(port,() => {
 
-    console.log(`Server running at port `+port);
+    console.log(`Server running at port `);
 
 });
